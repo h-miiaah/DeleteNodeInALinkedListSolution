cc8ff1c5c46c7c1bea6a5501c9a29be5dab0be04
h-miiaah <haim.miiaah5@gmail.com>
2022-03-29 22:43:46 -0400

    conditional

--- DeleteNodeInALinkedList.js
+++ DeleteNodeInALinkedList.js
@@ -10,5 +10,7 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
  var deleteNode = function(node) {
-    
+    if(node == null || node.next == null){
+       
+    }
 };
